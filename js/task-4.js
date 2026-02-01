a28fe34dc2b98b4c2f70f9fb0862df7f35c85da6
Захар <zohanimaa@gmail.com>
2026-02-01 16:56:22 +0200

    add tasks

--- js/task-4.js
+++ js/task-4.js
@@ -1,20 +1,43 @@
-function getShippingCost(country) {
-    switch (country) {
-        case 'China':
-    return 'Shipping to China will cost 100 credits';
-        case 'Chile':
-    return 'Shipping to Chile will cost 250 credits';
-        case 'Australia':
-    return 'Shipping to Australia will cost 170 credits';
-    case 'Jamaica':
-    return 'Shipping to Jamaica will cost 120 credits';
-    default:
-    return 'Sorry, there is no delivery to your country'
-    }
-}
-console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
-console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
-console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
-console.log(getShippingCost("Chile")); // "Shipping to Chile will cost 250 credits"
-console.log(getShippingCost("Jamaica")); // "Shipping to Jamaica will cost 120 credits"
-console.log(getShippingCost("Sweden")); // "Sorry, there is no delivery to your country"
+const getTotalBalanceByGender = (users, gender) => users.filter(user => user.gender === gender).map(user => user.balance).reduce((acc, balance) => acc + balance, 0);
+
+const clients = [
+	{
+    name: "Moore Hensley",
+    gender: "male",
+    balance: 2811
+  },
+  {
+    name: "Sharlene Bush",
+    gender: "female",
+    balance: 3821
+  },
+  {
+    name: "Ross Vazquez",
+    gender: "male",
+    balance: 3793
+  },
+  {
+    name: "Elma Head",
+    gender: "female",
+    balance: 2278
+  },
+  {
+    name: "Carey Barr",
+    gender: "male",
+    balance: 3951
+  },
+  {
+    name: "Blackburn Dotson",
+    gender: "male",
+    balance: 1498
+  },
+  {
+    name: "Sheree Anthony",
+    gender: "female",
+    balance: 2764
+  }
+];
+
+console.log(getTotalBalanceByGender(clients, "male")); // 12053
+
+console.log(getTotalBalanceByGender(clients, "female")); // 8863
